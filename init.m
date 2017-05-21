ae9f4d6d9d96bd54dab9b82ed45f5da43c25c4d9
mykolakolinko <mykolakolinko@gmail.com>
2017-05-21 18:17:32 +0300

    Update init.m

--- init.m
+++ init.m
@@ -2,19 +2,29 @@ function [] = init()
   #T - right endpoint, t - uniform partition of [0, T] with N points and diametr of partition dt
   T=1; N=101; dt=T/(N-1); t=0:dt:T;
   #number of iterations
-  n_iter = 1000;
+  n_iter = 30;
   
   #coefficients
-  x0 = 1.9;
-  a = 3.5;
+  x0 = 2;
+  a = -1;
   b = 0;
-  c = 0.8;
+  c = -1.3;
   d = 0;
   
   #solution
-  x = @(w)(x0*exp((a-c^2/2)*t+c*w));
-  #mean solution (if b=d=0)
-  mx = x0*exp(a*t);
+  if (b==0 & d==0) x = @(w)(x0*exp((a-c^2/2)*t+c*w));
+  elseif (b==0 & c==0)
+    x = @(w)(exp(a*t).*(cumsum(exp(-a*t).*[x0,diff(w)])));
+  else 
+    #general solution: x(1) = x0 for m = 2:N x(m) = @(w)(exp((a-c^2/2)*t(m)+c*w(m))*(x0 + (b-c*d)*sum(exp(-(a-c^2/2)*t(1:m)-c*w(1:m))*dt) + d*sum(exp(-(a-c^2/2)*t(1:m)-c*w(1:m)).*diff(w)))); end
+  endif
+  
+  #mean solution
+  if (b==0 & d==0) mx = x0*exp(a*t); 
+  elseif (b==0 & c==0) mx(1:N) = x0*exp(a*t);
+  else 
+    #general expectation:
+  endif
   
 	save par.mat
 end;
